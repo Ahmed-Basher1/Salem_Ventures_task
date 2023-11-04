52f3023dc65c1e459cd91611d47cd6e4885eb9a3
ahmed-basher2 <a7medbasher811@gmail.com>
2023-11-04 09:48:46 +0200

    add nest config for todo app

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types, HydratedDocument } from 'mongoose';
+
+// Define an enum for Task status if there are specific allowed values
+export enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+  // add other statuses if needed
+}
+
+@Schema()
+export class Task {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ enum: TaskStatus, default: TaskStatus.OPEN })
+  status: TaskStatus;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  userId: Types.ObjectId;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
+
+// This creates a TaskDocument type that includes mongoose document properties
+export type TaskDocument = HydratedDocument<Task>;
---
+++ src/users/user.model.ts
@@ -0,0 +1,52 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, HydratedDocument } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({
+    unique: true,
+    required: true,
+  })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  // Call this method to hash the password before saving the user
+  async hashPassword(): Promise<void> {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+
+  // Call this method to compare a password with the user's hashed password
+  async comparePassword(candidatePassword: string): Promise<boolean> {
+    return bcrypt.compare(candidatePassword, this.password);
+  }
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+// Ensure the password is hashed before saving the user document
+UserSchema.pre('save', async function (next) {
+  const user = this as UserDocument;
+
+  // Only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // Hash the password
+  await user.hashPassword();
+  next();
+});
+
+// Add comparePassword method to the user document
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string,
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export type UserDocument = HydratedDocument<User>;
